877aceb2aaa96bdc86e5c03f1a7addf23abbc790
Kaliszando <kaliszadam99@gmail.com>
2020-05-09 22:01:17 +0200

    Setting up project

---
+++ src/Controller/Main.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class Main {
+
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("GUI");
+		}
+		else {
+			System.out.println("Console");
+		}
+	}
+
+}
